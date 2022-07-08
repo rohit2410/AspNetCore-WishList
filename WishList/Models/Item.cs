e9d058afd1d7e5386c9154afe3dd0db9eada4d2b
Rohit <chauhan9911910013@gmail.com>
2022-07-08 16:21:28 +0000

    Module 4 completed

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [Required,MaxLength(50)]
+        public String Description { get; set; }
+    }
+}
