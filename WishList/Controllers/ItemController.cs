f2693c8e72197daf1e55b51ede618f29132a2adb
Rohit <chauhan9911910013@gmail.com>
2022-07-09 07:43:19 +0000

    Module 6  finished

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -37,7 +37,7 @@ namespace WishList.Controllers
            var item =  _context.Items.FirstOrDefault(e =>e.Id == Id);
             _context.Items.Remove(item);
             _context.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("Index","Item");
         }
     }
 }
