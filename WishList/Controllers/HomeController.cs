5412040c520c0ec2e548546cda16218e59ca67ef
Rohit <chauhan9911910013@gmail.com>
2022-07-10 06:26:05 +0000

    added index

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace WishList.Controllers
     {
         public IActionResult Index()
         {
-            return View("");
+            return View("Index");
         }
 
         public IActionResult Error()
